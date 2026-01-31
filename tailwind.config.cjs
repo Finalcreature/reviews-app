883d4f29e2666345abb59e7dfd59b3631f17fa8e
Finalcreature <finalnadav@gmail.com>
2026-01-31 22:59:27 +0200

    Changed tailwind cdn to a local setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './index.html',
+    './App.{js,jsx,ts,tsx}',
+    './index.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+    './Pages/**/*.{js,jsx,ts,tsx}',
+    './hooks/**/*.{js,jsx,ts,tsx}',
+    './services/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
